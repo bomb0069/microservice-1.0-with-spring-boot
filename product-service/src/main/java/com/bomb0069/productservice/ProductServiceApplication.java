9b8db3a72c12eb41272a0d7eb982b15cb0ace8ec
Karan Sivrat <bomb0069@gmail.com>
2018-12-05 16:06:12 +0700

    create file for product service

---
+++ product-service/src/main/java/com/bomb0069/productservice/ProductServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bomb0069.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+}
